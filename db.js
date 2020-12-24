23c9feeabc122ffeffd80b8dfdbf1c35196e7f31
Ilia Odintsov <gofr.one@gmail.com>
2020-12-24 14:51:22 +0300

    Running SELECT statement

--- db.js
+++ db.js
@@ -6,8 +6,40 @@ var Connection = require('tedious').Connection;
 var config = require('./.env/config');
 var connection = new Connection(config);  
 
-connection.on('connect', function(err) {  
-    // If no error, then good to proceed.
-    console.log("Connected");  
+// Attempt to connect and execute queries if connection goes through
+connection.connect( err => {
+    if (err) {
+        console.error(err.message);
+    } else {
+        queryDatabase();
+    }
 });
 
+var Request = require('tedious').Request;  
+//var TYPES = require('tedious').TYPES;  
+
+function queryDatabase() {
+    console.log("Reading rows from the Table...");
+  
+    // Read all rows from table
+    const request = new Request(
+        `SELECT DB_NAME() [db], 
+                CURRENT_TIMESTAMP [current_time], 
+                @@VERSION [sql_version];`,
+        (err, rowCount) => {
+            if (err) {
+                console.error(err.message);
+            } else {
+                console.log(`${rowCount} row(s) returned`);
+            }
+        }
+    );
+  
+    request.on("row", columns => {
+        columns.forEach(column => {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+        });
+    });
+  
+    connection.execSql(request);
+}
