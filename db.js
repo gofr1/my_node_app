92f3f9c169d707e622d782f5edf11a5de7a20220
Ilia Odintsov <gofr.one@gmail.com>
2020-12-24 14:14:34 +0300

    Establishing connection

--- db.js
+++ db.js
@@ -1,3 +1,13 @@
 // Install tedious module in your project. 
 // Tedious is the implementation of the TDS protocol, which is used to communicate to SQL Server.
 //* npm install tedious
+
+var Connection = require('tedious').Connection;
+var config = require('./.env/config');
+var connection = new Connection(config);  
+
+connection.on('connect', function(err) {  
+    // If no error, then good to proceed.
+    console.log("Connected");  
+});
+
